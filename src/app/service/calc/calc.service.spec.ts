74a05ef44afb1c937fd24715c86711860c3f8994
anweress <chlayfa.anouar@gmail.com>
2026-03-06 01:38:16 +0100

    2.1  spyOn

--- src/app/service/calc/calc.service.spec.ts
+++ src/app/service/calc/calc.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import {SharedService} from '../shared/shared.service';
 import {CalcService} from './calc.service';
 
 
 describe('CalcService', () => {
 
-  it ("should multiply two numbers", () => {
-    const calcService = new CalcService();
-    const result = calcService.multiply(3, 5);
-    expect(result).toBe(15);
-  })
+  // it ("should multiply two numbers", () => {
+  //   const sharedService = new SharedService();
+  //   const calcService = new CalcService(sharedService);
+  //   const result = calcService.multiply(3, 5);
+  //   expect(result).toBe(15);
+  // });
+
+
+  it("should call mySharedFunction func", () => {
+    const shared = new SharedService();
+    const calc = new CalcService(shared);
+    spyOn(shared, 'mySharedFunction'); // It creates a spy and It replaces the original function with an empty fake function
+    // spyOn(shared, 'mySharedFunction').and.callThrough(); // the spy tracks the function, the real implementation still runs
+    const result = calc.multiply(3, 5);
+    expect(shared.mySharedFunction).toHaveBeenCalled();
+  });
 
 });
