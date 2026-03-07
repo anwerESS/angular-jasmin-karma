63920150b282b7b4d08f26e567f73f56b4cc9512
anweress <chlayfa.anouar@gmail.com>
2026-03-07 21:06:02 +0100

    5.3 Testing dom elements

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,43 +1,33 @@
-// Import Angular testing utilities
-// ComponentFixture is the testing wrapper around a component
-// TestBed creates a fake Angular module for the test environment
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-// Import the component we want to test
 import { HomeComponent } from './home.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
-// Define a test suite for HomeComponent
 describe('HomeComponent', () => {
-  // Variable that will store the component class instance
   let component: HomeComponent;
-  // Variable that will store the fixture (test environment for the component)
   let fixture: ComponentFixture<HomeComponent>;
-  // Runs before each test
-  beforeEach(async () => {
-    // Configure a temporary Angular testing module
-    // Similar to a mini AppModule used only for the test
-    await TestBed.configureTestingModule({
-      // Because HomeComponent is a standalone component
-      // we import it instead of declaring it
-      imports: [HomeComponent]
-    })
-      // Compile the component template and styles
-      // Angular prepares the component for rendering
-      .compileComponents();
+  let el: DebugElement;
 
-    // Create the component instance inside the testing environment
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomeComponent]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
-    // Get the component class instance from the fixture
     component = fixture.componentInstance;
-    // Trigger Angular change detection
-    // This renders the template and runs lifecycle hooks like ngOnInit()
-    fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
 
-  // Define a test case
   it('should create', () => {
-    // Verify that the component instance exists
     expect(component).toBeTruthy();
   });
+
+  it('Should have correct content', () => {
+    let pEl = el.queryAll(By.css('p'));
+    expect(pEl[0].nativeElement.textContent).toBe("home works!");
+    let btnEl = el.queryAll(By.css('.btn'));
+    expect(btnEl[0].nativeElement.disabled).toBeTrue();
+  })
 
 });
